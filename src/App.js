c3f166fcd3ebcf80b9d752a90bc898d4f55485e8
Busari Moruf <busarimoruf20@gmail.com>
2023-10-25 22:47:06 +0100

    new changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <h1>Hello world!</h1>
-  );
+    <>
+      <Header />
+    </>
+  )
 }
 
 export default App;
