035bb07c7677817fff5f5ee6888b032dc581bfba
Busari Moruf <busarimoruf20@gmail.com>
2023-10-26 23:48:05 +0100

    new changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-
 import './App.css';
-
 import Header from "./components/Header/Header";
+import Hero from "./components/UI/Hero";
+import Counter from './components/UI/Counter';
 
 function App() {
   return (
     <>
       <Header />
+      <Hero />
+      <Counter />
     </>
-  )
+  );
 }
 
 export default App;
---
+++ src/components/UI/Counter.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../../styles/counter.css'
+
+const Counter = () => {
+  return (
+    <section className="counter">
+        <div className="container">
+            <div className="counter__wrapper">
+                <div className="counter__item">
+                    <h3 className="counter__number">5k+</h3>
+                     <h4 className="counter__title">Clients</h4>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Counter
