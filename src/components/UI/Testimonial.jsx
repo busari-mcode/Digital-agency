bb635be3abc4b7136504f2e5b8df728fef5309e2
Busari Moruf <busarimoruf20@gmail.com>
2023-10-31 19:53:44 +0100

    new changes

--- src/components/UI/Testimonial.jsx
+++ src/components/UI/Testimonial.jsx
@@ -2,38 +2,92 @@ import React from 'react';
 import '../../styles/testimonial.css';
 import Slider from 'react-slick';
 import ava01 from '../../images/ava-1.jpg';
-import ava02 from '../../images/ava-1.jpg';
-import ava03 from '../../images/ava-1.jpg';
+
 
 const Testimonial = () => {
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 1000,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    }
   return (
     <section>
         <div className="container">
             <div className="slider__content-top">
                 <h6 className="subtitle">Testimonials</h6>
-                <h2>Trusted by more than <span className="highlight">5,000 customers</span></h2>
+                <h2>Trusted by more than{" "}<span className="highlight">5,000 customers</span></h2>
             </div>
 
             <div className="slider__wrapper">
-                <div className="slider__item">
-                <p className="description">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                    Expedita molestiae facilis laboriosam est atque! Eaque sit 
-                    modi repellat dolor eos, minima ipsa odio maiores error quas 
-                    saepe repellendus dignissimos sequi voluptates aspernatur 
-                    adipisci. Facere dolorem provident doloremque fugiat quas hic!
-                </p>
+                <Slider {...settings}>
+                    <div className="slider__item">
+                     <p className="description">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                        Expedita molestiae facilis laboriosam est atque! Eaque sit 
+                        modi repellat dolor eos, minima ipsa odio maiores error quas 
+                        saepe repellendus dignissimos sequi voluptates aspernatur 
+                        adipisci. Facere dolorem provident doloremque fugiat quas hic!
+                     </p>
 
-                <div className="customer__details">
-                    <div className="customer__img">
-                        <img src={ava01} alt="" />
+                     <div className="customer__details">
+                        <div className="customer__img">
+                            <img src={ava01} alt="" />
+                        </div>
+
+                        <div>
+                            <h5 className="customer__name">Jhon Doe</h5>
+                            <p className="description">CEO, Workcreation</p>
+                        </div>
+                     </div>
                     </div>
 
-                    <div>
-                        <h5 className="customer__name">Jhon Doe</h5>
+                    <div className="slider__item">
+                     <p className="description">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                        Expedita molestiae facilis laboriosam est atque! Eaque sit 
+                        modi repellat dolor eos, minima ipsa odio maiores error quas 
+                        saepe repellendus dignissimos sequi voluptates aspernatur 
+                        adipisci. Facere dolorem provident doloremque fugiat quas hic!
+                     </p>
+
+                     <div className="customer__details">
+                        <div className="customer__img">
+                            <img src={ava01} alt="" />
+                        </div>
+
+                        <div>
+                            <h5 className="customer__name">Jhon Doe</h5>
+                            <p className="description">CEO, Workcreation</p>
+                        </div>
+                     </div>
                     </div>
-                </div>
-                </div>
+
+                    <div className="slider__item">
+                     <p className="description">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                        Expedita molestiae facilis laboriosam est atque! Eaque sit 
+                        modi repellat dolor eos, minima ipsa odio maiores error quas 
+                        saepe repellendus dignissimos sequi voluptates aspernatur 
+                        adipisci. Facere dolorem provident doloremque fugiat quas hic!
+                     </p>
+
+                     <div className="customer__details">
+                        <div className="customer__img">
+                            <img src={ava01} alt="" />
+                        </div>
+
+                        <div>
+                            <h5 className="customer__name">Jhon Doe</h5>
+                            <p className="description">CEO, Workcreation</p>
+                        </div>
+                     </div>
+                    </div>
+                </Slider>
             </div>
         </div>
     </section>
